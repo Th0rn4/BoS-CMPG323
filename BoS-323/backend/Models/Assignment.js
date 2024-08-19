9e8777504535b2b610d655b4a7ed062106076124
DanieNieb <danieniebuhr@gmail.com>
2024-08-19 21:44:10 +0200

    Assignment Model
    Data Scheme, Still needs some work

--- BoS-323/backend/Models/Assignment.js
+++ BoS-323/backend/Models/Assignment.js
@@ -1,1 +1,11 @@
 // Defines the Assignment schema and model
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AssignmentSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  dueDate: { type: Date, required: true },
+  course: { type: String, ref: 'Course', required: true },
+  submissions: [{ type: String, ref: 'Submission' }],
+});
