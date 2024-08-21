4480ec729f194dde1dbf9d69c764a73a8aa27a70
IngeSenekal <senekalinge@gmail.com>
2024-08-21 11:45:54 +0200

    Created Schema for Submissions
    Also added into controller, routes and services

--- BoS-323/backend/Controllers/SubmissionController.js
+++ BoS-323/backend/Controllers/SubmissionController.js
@@ -1,1 +1,23 @@
 // Manages submission-related actions (e.g., submit, update, grade)
+const {
+  createSubmission,
+  getSubmissions,
+} = require("../Services/submissionServices");
+
+exports.createSubmission = async (req, res) => {
+  try {
+    const submission = await createSubmission(req.body);
+    res.status(201).json(submission);
+  } catch (error) {
+    res.status(500).json({ message: "Error creating submission", error });
+  }
+};
+
+exports.getSubmissions = async (req, res) => {
+  try {
+    const submissions = await getSubmissions();
+    res.status(200).json(submissions);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching submissions", error });
+  }
+};
--- BoS-323/backend/server.js
+++ BoS-323/backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./Config/db");
 const userRoutes = require("./Routes/userRoutes");
+const subRoutes = require("./Routes/submissionRoutes");
 
 const app = express();
 
@@ -14,6 +15,7 @@ connectDB();
 
 // Routes
 app.use("/api/users", userRoutes);
+app.use("/api/submissions", subRoutes);
 
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
