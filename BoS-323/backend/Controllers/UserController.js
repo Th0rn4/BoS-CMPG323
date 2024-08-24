94c07aae4f47f12777e6877523b2b3b52d71ab2a
Reuben <reuben.j.thorn@gmail.com>
2024-08-24 21:55:33 +0200

    Encrypted Passwords
    Added encrypted passwords for users using argon2 API

--- BoS-323/backend/Controllers/UserController.js
+++ BoS-323/backend/Controllers/UserController.js
@@ -8,6 +8,7 @@ const { ObjectId } = require("mongoose").Types;
 exports.createUser = async (req, res) => {
   try {
     const user = await createUser(req.body);
+    await user.save();
     res.status(201).json(user);
   } catch (error) {
     res.status(500).json({ message: "Error creating user", error });
