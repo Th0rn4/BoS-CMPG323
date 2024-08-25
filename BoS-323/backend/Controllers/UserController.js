b353bbea01838b698f2e579327ac56e83f07ff86
Reuben <reuben.j.thorn@gmail.com>
2024-08-25 16:02:31 +0200

    Added get a single user by ID

--- BoS-323/backend/Controllers/UserController.js
+++ BoS-323/backend/Controllers/UserController.js
@@ -25,6 +25,27 @@ exports.getUsers = async (req, res) => {
   }
 };
 
+exports.getUserById = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+      return res.status(400).json({ message: "Invalid user ID" });
+    }
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.error("Error in getUserById:", error);
+    res
+      .status(500)
+      .json({ message: "Error fetching user", error: error.message });
+  }
+};
+
 //U
 exports.updateUser = async (req, res) => {
   try {
--- BoS-323/backend/Routes/userRoutes.js
+++ BoS-323/backend/Routes/userRoutes.js
@@ -6,6 +6,7 @@ const UserController = require("../Controllers/UserController");
 //CRUD
 router.post("/create", UserController.createUser);
 router.get("/", UserController.getUsers);
+router.get("/:id", UserController.getUserById);
 router.put("/update/:id", UserController.updateUser);
 router.delete("/delte/:id", UserController.deleteUser);
 
