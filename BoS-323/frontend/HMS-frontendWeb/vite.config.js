075cdec5fb584b4e09eabb6982df2cb38f217034
DanieNieb <75256839+DanieNieb@users.noreply.github.com>
2024-10-16 18:25:05 +0200

    Test1

--- BoS-323/frontend/HMS-frontendWeb/vite.config.js
+++ BoS-323/frontend/HMS-frontendWeb/vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    open: true,
+    host: '0.0.0.0', // Listen on all addresses
+    port: process.env.PORT || 3000, // Use the PORT environment variable or fallback to 3000
   },
   build: {
-    outDir: "dist",
+    outDir: 'dist',
     sourcemap: true,
   },
 });
